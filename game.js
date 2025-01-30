997547e296d4134ae5698abee5959221f09a6992
Oussama Lejmi <oussama.lejmi@insat.ucar.tn>
2025-01-30 17:41:58 +0100

    Add a function that allows the user to play one round

--- game.js
+++ game.js
@@ -1,3 +1,11 @@
+let humanScore = 0;
+let ComputerScore = 0;
+
+let humanChoice = getHumanChoice();
+let computerChoice = getComputerChoice();
+
+playRound(humanChoice, computerChoice);
+
 function getComputerChoice() {
   randomNumber = Math.ceil(Math.random() * 3);
   switch (randomNumber) {
@@ -17,4 +25,38 @@ function getHumanChoice() {
     option = prompt("Please type one of the following options: rock, paper, or scissors");
   }
   return option;
+}
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice == "rock" && computerChoice == "paper") {
+    console.log("You lose!Paper beats Rock");
+    ComputerScore++;
+  } else if (humanChoice == "rock" && computerChoice == "scissors") {
+    console.log("You win!Rock beats Scissors");
+    humanScore++;
+  }
+  if (humanChoice == "rock" && computerChoice == "rock") {
+    console.log("It's a draw!");
+  }
+  if (humanChoice == "paper" && computerChoice == "rock") {
+    console.log("You win!Paper beats Rock");
+    humanScore++;
+  }
+  if (humanChoice == "paper" && computerChoice == "scissors") {
+    console.log("You lose!Scissors beats Paper");
+    ComputerScore++;
+  }
+  if (humanChoice == "paper" && computerChoice == "paper") {
+    console.log("It's a draw!");
+  }
+  if (humanChoice == "scissors" && computerChoice == "paper") {
+    console.log("You win!Scissors beats Paper");
+    humanScore++;
+  }
+  if (humanChoice == "scissors" && computerChoice == "rock") {
+    console.log("You lose!Rock beats Scissors");
+    ComputerScore++;
+  }
+  if (humanChoice == "scissors" && computerChoice == "scissors") {
+    console.log("It's a draw!");
+  }
 }
